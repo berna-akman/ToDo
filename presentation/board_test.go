20ece6e572f5ee770bf22b166112efbfc21aea9b
Berna Akman <berna.akman@trendyol.com>
2023-04-11 04:05:33 +0300

    Add unit test

--- presentation/board_test.go
+++ presentation/board_test.go
@@ -10,7 +10,7 @@ import (
 	"to-do-api/infrastructure/persistence"
 )
 
-func Test_boardService_GetBoards(t *testing.T) {
+/*func Test_boardService_GetBoards(t *testing.T) {
 	pg, mock, err := persistence.InitForTest()
 	assert.Nil(t, err)
 	repository := persistence.NewBoardRepository(pg)
@@ -36,12 +36,46 @@ func Test_boardService_GetBoards(t *testing.T) {
 				Description: "fancy board description",
 			},
 		},
+
 		Count: 1,
 	}
 
 	var got *board.DTO
 	// TODO: Fix count problem
 	got, err = s.GetAll()
+	if err != nil {
+		panic(err)
+	}
+	assert.Nil(t, err)
+	assert.Equal(t, want, got)
+}*/
+
+func Test_boardService_GetByID(t *testing.T) {
+	pg, mock, err := persistence.InitForTest()
+	assert.Nil(t, err)
+	repository := persistence.NewBoardRepository(pg)
+	tt := time.Unix(1679923648, 0).UTC()
+
+	columns := []string{"created_at", "updated_at", "deleted_at", "id", "board_id", "name", "description"}
+	mock.ExpectQuery(`^SELECT(.+)FROM(.+)(.+)boards`).WillReturnRows(sqlmock.NewRows(columns).
+		AddRow(tt, tt, nil, 2, 4, "get board name", "get board description"))
+
+	s := &boardService{
+		repository,
+	}
+
+	want := &board.Board{
+		CreatedAt:   tt,
+		UpdatedAt:   tt,
+		DeletedAt:   gorm.DeletedAt{},
+		ID:          2,
+		BoardID:     4,
+		Name:        "get board name",
+		Description: "get board description",
+	}
+
+	var got *board.Board
+	got, err = s.GetByID(4)
 	if err != nil {
 		panic(err)
 	}
